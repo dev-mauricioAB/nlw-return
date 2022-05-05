29be15ffe9c85a4a750040e070cda115fd1abdc8
MauricioAB <dev.mauricioalezandre@gmail.com>
2022-05-05 06:49:02 -0300

    adding route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,26 @@
 import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
+app.use(express.json());
+
+
+app.get('feedbacks', () => {
+
+});
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  })
+
+  return res.status(201).json({ data: feedback });
+})
 
 app.listen(3333, () => console.log('Bombo 💣 '));
