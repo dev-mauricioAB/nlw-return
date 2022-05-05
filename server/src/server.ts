c8fe8423f72d901d879074668982a56622a1526c
MauricioAB <dev.mauricioalezandre@gmail.com>
2022-05-05 07:07:16 -0300

    adding send mail logic

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,21 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 const app = express();
 app.use(express.json());
 
-
 app.get('feedbacks', () => {
 
+});
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "5b1da54dd06239",
+    pass: "3b91316b06bb4a"
+  }
 });
 
 app.post('/feedbacks', async (req, res) => {
@@ -18,7 +27,19 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot
     }
-  })
+  });
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Mauricio Barroso <dev.mauricioalezandre@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}<p/>`,
+      `<p>Comentario: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
+  });
 
   return res.status(201).json({ data: feedback });
 })
